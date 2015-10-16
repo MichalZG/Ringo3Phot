a5633e4c2bc129d966b38e4856a563620eca0391
misza314 <michalzejmo@gmail.com>
2015-10-16 13:36:27 +0200

    fix bug in linkAll

--- linkAll.py
+++ linkAll.py
@@ -6,28 +6,29 @@ import fnmatch
 
 ext = '*.fits'
 outDirName = 'results'
-scriptPath = os.path.curdir
+workPath = os.getcwd()
 
 
 def find(pattern, path):
     result = []
-    for root, dirs, files in os.walk(path, topdown=True):
+    for root, dirs, files in os.walk(path):
         dirs[:] = [d for d in dirs if (d.isdigit() and d.__len__() == 8)]
         for name in files:
             if fnmatch.fnmatch(name, pattern):
-                result.append(os.path.join(root, name))
+                result.append(os.path.join(workPath, root, name))
     return result
 
 
 try:
-    os.mkdir(os.path.join(scriptPath, outDirName))
+    os.mkdir(os.path.join(workPath, outDirName))
 except OSError:
-    filesToRemove = glob.glob(os.path.join(scriptPath, outDirName, ext))
+    filesToRemove = glob.glob(os.path.join(workPath, outDirName, ext))
     for f in filesToRemove:
         os.remove(f)
 
-fileList = find(ext, scriptPath)
+fileList = find(ext, workPath)
 for fileToLink in fileList:
     fileName = fileToLink.split('/')[-1]
+    print fileToLink
     os.symlink(fileToLink,
-               os.path.join(scriptPath, outDirName, fileName))
+               os.path.join(workPath, outDirName, fileName))
