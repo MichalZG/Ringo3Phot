807984219bc4b63b71cb177f8ef27f929debda21
misza314 <michalzejmo@gmail.com>
2015-10-16 12:26:24 +0200

    add linkAll

---
+++ linkAll.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import glob
+import os
+import fnmatch
+
+
+ext = '*.fits'
+outDirName = 'results'
+scriptPath = os.path.curdir
+
+
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+
+try:
+    os.mkdir(os.path.join(scriptPath, outDirName))
+except OSError:
+    filesToRemove = glob.glob(os.path.join(scriptPath, outDirName, ext))
+    for f in filesToRemove:
+        os.remove(f)
+
+fileList = find(ext, scriptPath)
+for fileToLink in fileList:
+    fileName = fileToLink.split('/')[-1]
+    os.symlink(fileToLink,
+               os.path.join(scriptPath, outDirName, fileName))
