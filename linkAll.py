7c75bef21ba2837414a78a0c1c71cab4d8dbc250
misza314 <michalzejmo@gmail.com>
2015-10-16 13:16:05 +0200

    fix bug in linkAll

--- linkAll.py
+++ linkAll.py
@@ -11,7 +11,8 @@ scriptPath = os.path.curdir
 
 def find(pattern, path):
     result = []
-    for root, dirs, files in os.walk(path):
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs[:] = [d for d in dirs if (d.isdigit() and d.__len__() == 8)]
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
